d646b71f22146e409e685f1e29daf57d2f7e4a38
Lunax00 <nada1nadiri@gmail.com>
2025-10-08 22:33:37 +0100

    v1
    d

--- video-service/src/main/java/ma/xproce/videoservice/VideoServiceApplication.java
+++ video-service/src/main/java/ma/xproce/videoservice/VideoServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VideoServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoServiceApplication.class, args);
+        
+        SpringApplication.run(VideoServiceApplication.class, args);
 	}
 
 }
