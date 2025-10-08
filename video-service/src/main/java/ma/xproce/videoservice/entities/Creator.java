d17700c5531ec201fe9b6002aeb1024989856e36
Lunax00 <nada1nadiri@gmail.com>
2025-10-09 00:23:41 +0100

    v

---
+++ video-service/src/main/java/ma/xproce/videoservice/entities/Creator.java
@@ -0,0 +1,26 @@
+package ma.xproce.videoservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Creator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
+    private List<Video> videos;
+
+}
---
+++ video-service/src/main/java/ma/xproce/videoservice/entities/Video.java
@@ -0,0 +1,25 @@
+package ma.xproce.videoservice.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String url;
+    private String description;
+    private Date datePublication;
+    @ManyToOne
+    private Creator creator;
+}
