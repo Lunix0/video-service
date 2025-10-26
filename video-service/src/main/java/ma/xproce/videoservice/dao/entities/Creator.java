a6ab06a515598571f9de7a958e97fd68b35edfb7
Lunax00 <nada1nadiri@gmail.com>
2025-10-26 23:45:18 +0100

    Update Creator.java

--- video-service/src/main/java/ma/xproce/videoservice/dao/entities/Creator.java
+++ video-service/src/main/java/ma/xproce/videoservice/dao/entities/Creator.java
@@ -19,6 +19,7 @@ public class Creator {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
+
     private String name;
     private String email;
     @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
