d1e0b00b85963ebb3c2d394845f21425f6637f92
Lunax00 <nada1nadiri@gmail.com>
2025-10-29 23:45:36 +0100

    Update CalculatriceAire.java

--- Mockito/src/main/java/ma/xproce/CalculatriceAire.java
+++ Mockito/src/main/java/ma/xproce/CalculatriceAire.java
@@ -6,7 +6,10 @@ public class CalculatriceAire {
     public static double aire(List<IForme> formes) {
         double aire = 0;
         for (IForme forme : formes) {
+
             aire += forme.aire();
+
+            
         }
 
         return aire;
